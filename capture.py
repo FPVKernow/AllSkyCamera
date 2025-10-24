6ddd5bc0c22d57704421e907319f8261c42b4b97
Tom Winterton <tomwinterton0@gmail.com>
2025-10-24 21:28:37 +0100

    bash script dependency and updated python script

--- capture.py
+++ capture.py
@@ -1,1 +1,9 @@
-print("hello, world");
+from time import sleep;
+from picamera import Picamera; # type: ignore
+
+camera = Picamera();
+camera.resolution(3040,3040);
+camera.start_preview();
+sleep(2);
+
+camera.capture('python.jpg');
