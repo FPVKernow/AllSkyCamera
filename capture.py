91df1b8cac7171b3184c1f7e33384918105db91b
Tom Winterton <tomwinterton0@gmail.com>
2025-10-29 15:51:28 +0000

    capture.py

--- capture.py
+++ capture.py
@@ -6,8 +6,9 @@ picam2 = Picamera2()
 picam2.start_preview(Preview.QTGL)
 
 preview_config = picam2.create_preview_configuration(main={"size":(3040,3040)})
+capture_config = picam2.create_still_configuration()
 
 picam2.configure(preview_config)
 
-picam2.start()
+picam2.start(capture_config)
 time.sleep(20)
