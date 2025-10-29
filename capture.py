c0d1c4736148e2bcdc7b8d988f2ffd1239f061e1
Tom Winterton <tomwinterton0@gmail.com>
2025-10-29 14:26:40 +0000

    updated main's files

--- capture.py
+++
@@ -1,9 +0,0 @@
-from time import sleep;
-from picamera import Picamera; # type: ignore
-
-camera = Picamera();
-camera.resolution(3040,3040);
-camera.start_preview();
-sleep(2);
-
-camera.capture('python.jpg');
