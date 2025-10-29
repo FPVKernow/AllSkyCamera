ac0df811a3a7bea070f07cd7285eec0c2a62f586
Tom Winterton <tomwinterton0@gmail.com>
2025-10-29 14:58:08 +0000

    capture.py

--- capture.py
+++ capture.py
@@ -1,10 +1,8 @@
 from picamera2 import Picamera2, Preview
-import time
+
+import datetime
+now = datetime.datetime.now()
 
 picam2 = Picamera2
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
-picam2.start_preview(Preview.DRM)
-picam2.start
-time.sleep(20)
-picam2.capture_file("cupboard.jpg")
+
+picam2.start_and_capture_file(now)
