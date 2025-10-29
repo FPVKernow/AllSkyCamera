8805f06c31a93093c13ebde1acc853bc2d0ec77a
Tom Winterton <tomwinterton0@gmail.com>
2025-10-29 16:05:18 +0000

    capture.py

--- capture.py
+++ capture.py
@@ -2,15 +2,18 @@ import time
 from picamera2 import Picamera2, Preview
 
 picam2 = Picamera2()
-
 picam2.start_preview(Preview.QTGL)
+preview_config = picam2.create_preview_configuration(main={"size":(100,100)})
 
-preview_config = picam2.create_preview_configuration(main={"size":(3040,3040)})
 capture_config = picam2.create_still_configuration()
 
 picam2.configure(preview_config)
-
 picam2.start()
-time.sleep(10)
+time.sleep(2)
 
-picam2.switch_mode_and_capture_file(capture_config, "fullres.jpg")
+image = picam2.switch_mode_and_capture_image(capture_config)
+image.show()
+
+time.sleep(5)
+
+picam2.close()
