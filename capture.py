1c9ca3616567707285740dc3944e9bf09df5e61c
Tom Winterton <tomwinterton0@gmail.com>
2025-10-29 14:53:30 +0000

    capture.py

--- capture.py
+++ capture.py
@@ -4,7 +4,7 @@ import time
 picam2 = Picamera2
 camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config)
-picam2.start_preview(Preview.QTGL)
+picam2.start_preview(Preview.DRM)
 picam2.start
 time.sleep(20)
 picam2.capture_file("cupboard.jpg")
