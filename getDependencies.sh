a77edf2fdf4b457950cadd64bd8e651887d02d2e
Tom Winterton <tomwinterton0@gmail.com>
2025-10-29 13:38:47 +0000

    added git to getdeps

--- getDependencies.sh
+++ getDependencies.sh
@@ -3,6 +3,7 @@ chmod u+x getDependencies.sh
 
 echo "Getting dependencies for AllSkyCamera"
 
+sudo apt-get install git
 sudo apt-get install python3-pip
 sudo apt-get install python-picamera
 sudo apt-get install python3-picamera
