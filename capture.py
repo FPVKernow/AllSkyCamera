35d151e444ef3c79320dac7d4b63cde36025ddad
Tom Winterton <tomwinterton0@gmail.com>
2025-10-29 15:32:34 +0000

    capture.py

--- capture.py
+++ capture.py
@@ -3,7 +3,7 @@ from picamera2 import Picamera2, Preview
 
 picam2 = Picamera2()
 
-picam2.start_preview(Preview.QTGL)
+picam2.start_preview(Preview.QTGL, x=100, y=100)
 
 preview_config = picam2.create_preview_configuration(main={"size":(1200,1200)})
 
