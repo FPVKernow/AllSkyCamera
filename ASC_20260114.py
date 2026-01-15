6ba576daf35c572facd5b32dc6d0778670db4d84
Tom Winterton <tomwinterton0@gmail.com>
2026-01-15 14:02:29 +0000

    improved user input handling

--- ASC_20260114.py
+++ ASC_20260114.py
@@ -13,19 +13,17 @@ print("AllSkyCamera CLI")
 def runtimeInput(): #Creates function
     maximumTime = 60*24 #60mins x 24hrs = 1440
     timeInput = float(input(f"How many minutes to run? (maximum {maximumTime}) ").replace(" ","")) #Declares timeInput as input and removes string whitespace. Allows decimals.
-
     if not(int(timeInput) < int(maximumTime) and int(timeInput) > 0):  #Out of time bounds error handelling
-        print("Error! Time was out of bounds! Please enter a time between 0 and 1440 minutes.")
+        print("Error! Time was out of bounds! Please enter a time between 1 and 1440 minutes.")
         exit
         return False
     else:
-        print(":)")   
-        print(f"Requested: {timeInput} minutes")
-        return timeInput #returns user input for later use
+            print("Valid input")
+    return timeInput #returns user input for later use
 
 
 def makeDir():
-    todaysDirectory = f"{filePath}/{today.strftime("%Y-%m-%d")}" #String with same YYYY-MM-DD format
+    todaysDirectory = directoryDate()
     try:
         os.mkdir(todaysDirectory) #Makes directory called todaysDirectory
         print(f"Created directory: {todaysDirectory}")
@@ -35,8 +33,22 @@ def makeDir():
         print(f"An error occured: {e}")
     return todaysDirectory
     
-#Call Functions Here#
-if runtimeInput() == False: 
-    exit
-else:
-    print(f"Saving images to: {makeDir()}")#Calls and prints function makeDir
+def directoryDate():
+    newDirectory = f"{filePath}/{today.strftime("%Y-%m-%d")}" #String with same YYYY-MM-DD format
+    return newDirectory
+
+def totalImages(userInput):
+     imagesPerMin = 60 / 10 #how many images per minute
+     totalCapture = userInput * imagesPerMin
+     return totalCapture
+
+#Call Functions Here/Main code#
+
+userTimeInput = runtimeInput()
+
+totalToCapture = totalImages(userTimeInput)
+
+print(f"Capturing {totalToCapture} images requested over {userTimeInput} minutes")
+
+print(f"Saving images to: {makeDir()}")#Calls and prints function makeDir
+
